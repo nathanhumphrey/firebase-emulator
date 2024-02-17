dd4324fad47e6ce8e91206defb4849fce11e87ac
Nathan Humphrey <NathanH@nait.ca>
2024-02-17 15:46:47 -0700

    Update functions tests to jest

--- project/functions/index.js
+++ project/functions/index.js
@@ -11,62 +11,62 @@ const { onRequest, onCall } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
+// const functions = require("firebase-functions");
+// const admin = require("firebase-admin");
+// admin.initializeApp();
 
-/**
- * Simple HTTP function that returns the "?text" query parameter in the response text.
- */
-exports.simpleHttp = onRequest((request, response) => {
-    response.send(`text: ${request.query.text}`);
-});
+// /**
+//  * Simple HTTP function that returns the "?text" query parameter in the response text.
+//  */
+// exports.simpleHttp = onRequest((request, response) => {
+//     response.send(`text: ${request.query.text}`);
+// });
 
-/**
- * Simple callable function that adds two numbers.
- */
-exports.simpleCallable = onCall((data, ctx) => {
-    // This function implements addition (a + b = c)
-    const sum = data.a + data.b;
-    return {
-        c: sum,
-    };
-});
+// /**
+//  * Simple callable function that adds two numbers.
+//  */
+// exports.simpleCallable = onCall((data, ctx) => {
+//     // This function implements addition (a + b = c)
+//     const sum = data.a + data.b;
+//     return {
+//         c: sum,
+//     };
+// });
 
-/**
- * Firestore-triggered function which uppercases a string field of a document.
- */
-exports.firestoreUppercase = functions.firestore
-    .document("/lowercase/{doc}")
-    .onCreate(async (doc, ctx) => {
-        const docId = doc.id;
+// /**
+//  * Firestore-triggered function which uppercases a string field of a document.
+//  */
+// exports.firestoreUppercase = functions.firestore
+//     .document("/lowercase/{doc}")
+//     .onCreate(async (doc, ctx) => {
+//         const docId = doc.id;
 
-        const docData = doc.data();
-        const lowercase = docData.text;
+//         const docData = doc.data();
+//         const lowercase = docData.text;
 
-        const firestore = admin.firestore();
-        await firestore.collection("uppercase").doc(docId).set({
-            text: lowercase.toUpperCase(),
-        });
-    });
+//         const firestore = admin.firestore();
+//         await firestore.collection("uppercase").doc(docId).set({
+//             text: lowercase.toUpperCase(),
+//         });
+//     });
 
-/**
- * Auth-triggered function which writes a user document to Firestore.
- */
-exports.userSaver = functions.auth.user().onCreate(async (user, ctx) => {
-    const firestore = admin.firestore();
+// /**
+//  * Auth-triggered function which writes a user document to Firestore.
+//  */
+// exports.userSaver = functions.auth.user().onCreate(async (user, ctx) => {
+//     const firestore = admin.firestore();
 
-    // Make a document in the user's collection with everything we know about the user
-    const userId = user.uid;
-    const userRef = firestore.collection("users").doc(userId);
-    await userRef.set(user.toJSON());
-});
+//     // Make a document in the user's collection with everything we know about the user
+//     const userId = user.uid;
+//     const userRef = firestore.collection("users").doc(userId);
+//     await userRef.set(user.toJSON());
+// });
--- project/functions/test/functions.spec.js
+++ project/functions/test/functions.spec.js
@@ -1,7 +1,11 @@
-const { expect } = require('chai');
-const admin = require('firebase-admin');
+import { describe, test, expect, jest } from "@jest/globals";
+//import firebaseFunctionsTest from 'firebase-functions-test';
+import { logger } from "firebase-functions";
 
-process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080';
+// Import the exported function definitions from our functions/index.js file
+import { helloWorld } from "../index.js";
+
+process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
 
 // Initialize the firebase-functions-test SDK using environment variables.
 // These variables are automatically set by firebase emulators:exec
@@ -9,97 +13,130 @@ process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080';
 // This configuration will be used to initialize the Firebase Admin SDK, so
 // when we use the Admin SDK in the tests below we can be confident it will
 // communicate with the emulators, not production.
-const test = require('firebase-functions-test')({
-    projectId: 'demo-project',
+// const { wrap } = firebaseFunctionsTest({
+//   projectId: 'demo-project',
+// });
+
+describe("Cloud Functions", () => {
+  //   afterEach(() => {
+  //     // Restores the original behavior of the wrapped functions
+  //     wrap.cleanup();
+  //   });
+
+  test('helloWorld should return "Hello from Firebase!"', async () => {
+    const mockLog = jest.spyOn(logger, "info");
+    // Wrap the exported function to make it testable
+    //const wrapped = wrap(helloWorld);
+
+    const sendPromise = new Promise((resolve) => {
+      
+      // A fake response object, with a stubbed send() function which asserts that it is called
+      // with the right result
+      const res = {
+        send: (text) => {
+          resolve(text);
+        },
+      };
+
+      // Invoke function with our fake request and response objects.
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      helloWorld({}, res);
+    });
+
+    // Wait for the promise to be resolved and then check the sent text
+    const text = await sendPromise;
+    expect(text).toBe("Hello from Firebase!");
+
+    // wrapped(cloudEventPartial);
+    expect(mockLog).toBeCalledTimes(1);
+  });
 });
 
-// Import the exported function definitions from our functions/index.js file
-const myFunctions = require('../index');
+// OLD MOCHA/CHAI STYLE TESTS
+// describe('Unit tests', () => {
+//   after(() => {
+//     test.cleanup();
+//   });
 
-describe('Unit tests', () => {
-    after(() => {
-        test.cleanup();
-    });
+//   it('tests a simple HTTP function', async () => {
+//     // A fake request object, with req.query.text set to 'input'
+//     const req = { query: { text: 'input' } };
 
-    it('tests a simple HTTP function', async () => {
-        // A fake request object, with req.query.text set to 'input'
-        const req = { query: { text: 'input' } };
+//     const sendPromise = new Promise((resolve) => {
+//       // A fake response object, with a stubbed send() function which asserts that it is called
+//       // with the right result
+//       const res = {
+//         send: (text) => {
+//           resolve(text);
+//         },
+//       };
 
-        const sendPromise = new Promise((resolve) => {
-            // A fake response object, with a stubbed send() function which asserts that it is called
-            // with the right result
-            const res = {
-                send: (text) => {
-                    resolve(text);
-                }
-            };
+//       // Invoke function with our fake request and response objects.
+//       myFunctions.simpleHttp(req, res);
+//     });
 
-            // Invoke function with our fake request and response objects.
-            myFunctions.simpleHttp(req, res)
-        });
+//     // Wait for the promise to be resolved and then check the sent text
+//     const text = await sendPromise;
+//     expect(text).to.eq(`text: input`);
+//   });
 
-        // Wait for the promise to be resolved and then check the sent text
-        const text = await sendPromise;
-        expect(text).to.eq(`text: input`);
-    });
+//   it('tests a simple callable function', async () => {
+//     const wrapped = test.wrap(myFunctions.simpleCallable);
 
-    it('tests a simple callable function', async () => {
-        const wrapped = test.wrap(myFunctions.simpleCallable);
+//     const data = {
+//       a: 1,
+//       b: 2,
+//     };
 
-        const data = {
-            a: 1,
-            b: 2,
-        };
+//     // Call the wrapped function with data and context
+//     const result = await wrapped(data);
 
-        // Call the wrapped function with data and context
-        const result = await wrapped(data);
+//     // Check that the result looks like we expected.
+//     expect(result).to.eql({
+//       c: 3,
+//     });
+//   });
 
-        // Check that the result looks like we expected.
-        expect(result).to.eql({
-            c: 3,
-        });
-    });
+//   it('tests a Cloud Firestore function', async () => {
+//     const wrapped = test.wrap(myFunctions.firestoreUppercase);
 
-    it('tests a Cloud Firestore function', async () => {
-        const wrapped = test.wrap(myFunctions.firestoreUppercase);
+//     // Make a fake document snapshot to pass to the function
+//     const after = test.firestore.makeDocumentSnapshot(
+//       {
+//         text: 'hello world',
+//       },
+//       '/lowercase/foo'
+//     );
 
-        // Make a fake document snapshot to pass to the function
-        const after = test.firestore.makeDocumentSnapshot(
-            {
-                text: 'hello world',
-            },
-            '/lowercase/foo'
-        );
+//     // Call the function
+//     await wrapped(after);
 
-        // Call the function
-        await wrapped(after);
+//     // Check the data in the Firestore emulator
+//     const snap = await admin.firestore().doc('/uppercase/foo').get();
+//     expect(snap.data()).to.eql({
+//       text: 'HELLO WORLD',
+//     });
+//   }).timeout(5000);
 
-        // Check the data in the Firestore emulator
-        const snap = await admin.firestore().doc('/uppercase/foo').get();
-        expect(snap.data()).to.eql({
-            text: 'HELLO WORLD',
-        });
-    }).timeout(5000);
+//   it('tests an Auth function that interacts with Firestore', async () => {
+//     const wrapped = test.wrap(myFunctions.userSaver);
 
-    it('tests an Auth function that interacts with Firestore', async () => {
-        const wrapped = test.wrap(myFunctions.userSaver);
+//     // Make a fake user to pass to the function
+//     const uid = `${new Date().getTime()}`;
+//     const email = `user-${uid}@example.com`;
+//     const user = test.auth.makeUserRecord({
+//       uid,
+//       email,
+//     });
 
-        // Make a fake user to pass to the function
-        const uid = `${new Date().getTime()}`;
-        const email = `user-${uid}@example.com`;
-        const user = test.auth.makeUserRecord({
-            uid,
-            email,
-        });
+//     // Call the function
+//     await wrapped(user);
 
-        // Call the function
-        await wrapped(user);
+//     // Check the data was written to the Firestore emulator
+//     const snap = await admin.firestore().collection('users').doc(uid).get();
+//     const data = snap.data();
 
-        // Check the data was written to the Firestore emulator
-        const snap = await admin.firestore().collection('users').doc(uid).get();
-        const data = snap.data();
-
-        expect(data.uid).to.eql(uid);
-        expect(data.email).to.eql(email);
-    }).timeout(5000);
-});
+//     expect(data.uid).to.eql(uid);
+//     expect(data.email).to.eql(email);
+//   }).timeout(5000);
+// });
